0a1aee5a238e985491adbe4346f8cae894e98daa
ItoTsuzuya <tuzuya1220@gmail.com>
2025-09-05 22:40:33 +0900

    スタート画面から、全体画面への推移を作成

---
+++ src/Wholescreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Wholescreen.css';
+
+const Wholescreen = () => {
+  return (
+    <div className="wholescreen-bg">
+      <h1>ホーム画面</h1>
+      {/* 他のホーム画面要素をここに追加 */}
+    </div>
+  );
+};
+
+export default Wholescreen;
