a77be05ed2724dc1694d511971de53cce61f55cc
ItoTsuzuya <tuzuya1220@gmail.com>
2025-09-05 15:50:20 +0900

    Add/Update files in branck fusen-byTuzuya

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Splash from './Splash';
+import Card from "./Card";
 
 
 const App = () => {
@@ -8,6 +9,12 @@ const App = () => {
   return (
     <>
       {showMain ? <p>タイトル</p> : <Splash onFinish={() => setShowMain(true)} />}
+      <Card
+        title="React Card"
+        image="https://via.placeholder.com/300x200"
+        description="This is a customizable card component."
+        buttonText="Learn More"
+      />
     </>
   );
 };
--- src/main2.jsx
+++
@@ -1,9 +0,0 @@
-const Main = () => {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>ようこそ、メインページへ！</h1>
-    </div>
-  );
-};
-
-export default Main;
