86e14791853dfaa95757669f184f6d565f28ab63
YuhoTakagi <bv25049@shibaura-it.ac.jp>
2025-09-06 00:30:24 +0900

    3つのタブ分けされ、それぞれのページで重なってる付箋が時系順に表示される（スワイプでめくる、下に返信が出てくる、見た目の細かい表示（CSS）、投稿内容の取得、App2.jsxへの接続はまだ）

---
+++ src/Yaritoriposts.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { posts } from './karidata.jsx';
+import { filterByShape,flattenReplies } from "./postUtils.jsx";
+
+const shapes = ["square", "circle", "star", "heart"];
+const UserId = 1;
+
+export const YaritoriPosts = () => {
+
+    
+    return(
+
+    <div>
+        <h2>やり取りした投稿・返信</h2>
+        {shapes.map(shape => {
+            const shapePosts = filterByShape(posts,shape);
+            let relatedPosts = [];
+
+            shapePosts.forEach((post)=>{
+                const replies = flattenReplies(post);
+                const all = [post, ...replies];
+                const involved = all.filter((item)=>item.authorId===UserId);
+                relatedPosts.push( ...involved);
+            });
+            return(
+                <div key={shape} className="stack">
+                    {relatedPosts
+                    .sort((a,b)=>b.id - a.id)
+                    .map((post, index) => (
+                        <div key={post.id}
+                        className="stack-item"
+                        style={{top: '${index*10}px',zIndex:relatedPosts.length-index}}
+                        >
+                            <div className='{post-card ${shape}}'>{post.text}</div>
+                        </div>
+                    ))}
+                </div>
+            );
+        })}
+    </div>
+
+)
+}
+export default YaritoriPosts
