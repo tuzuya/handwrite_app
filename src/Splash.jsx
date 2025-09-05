6e21f25908af86f7f1d995925ffeb53e01a35721
ItoTsuzuya <tuzuya1220@gmail.com>
2025-09-05 19:07:31 +0900

    アイコンの追加とそのフェードアウト実装

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -15,7 +15,7 @@ const Splash = ({ onFinish }) => {
 
   return (
     <div className={`splash-container ${fadeOut ? 'fade-out' : ''}`}>
-      <img src="/react.svg" alt="Logo" className="logo" />
+      <img src="/tuduri-icon.png" alt="Logo" className="logo" />
     </div>
   );
 };
